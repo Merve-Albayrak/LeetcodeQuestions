c65f962e321dd34e2b9d7f0717de6677c4aaac71
Merve-Albayrak <63204791+Merve-Albayrak@users.noreply.github.com>
2023-02-26 14:30:39 +0300

    Update 1.TwoSum.cs

--- 1.TwoSum.cs
+++ 1.TwoSum.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace Leetcode
 {
-    internal class _1
+   public class _1
     {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int[] indexes= new int[2];
+
+            for(int i=0;i<nums.Length;i++)
+            {
+                int firstValue = nums[i];
+                for (int x = i+1; x < nums.Length; x++)
+                {
+                    int secondValue = nums[x ];
+                    int result = firstValue + secondValue;
+                    if (result == target)
+                    {
+                        indexes[0] = i;
+                        indexes[1] = x;
+                        break;
+                    }
+                }
+               
+             
+               
+
+            }
+           return indexes;
+        }
     }
 }
