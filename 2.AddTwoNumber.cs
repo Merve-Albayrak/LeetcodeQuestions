ff64e58743b60dc0602009c48afb9df43eae2ba6
Merve-Albayrak <63204791+Merve-Albayrak@users.noreply.github.com>
2023-04-30 10:32:48 +0300

    Update 2.AddTwoNumber.cs

--- 2.AddTwoNumber.cs
+++ 2.AddTwoNumber.cs
@@ -8,5 +8,48 @@ namespace Question2_AddTwoNumbers
 {
     internal class AddTwoNumbers
     {
+         ListNode head = new ListNode(0);
+            ListNode current = head;
+            int carry = 0;
+            int sum = 0;
+            while (l1 != null || l2 != null || carry != 0)
+            {
+
+
+
+
+
+                sum =
+                   (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + carry;
+
+                int val = sum;
+
+                if (sum >= 10)
+                {
+
+
+                    int realVal = sum % 10;
+                    val = realVal;
+                    //   listNode=new ListNode(0);
+                    current.next = new ListNode(val);
+                }
+                else
+                {
+
+                    current.next = new ListNode(val);
+                }
+
+                // current.next=new ListNode(val);
+                current = current.next;
+                carry = sum / 10;
+                if (l1 != null)
+                    l1 = l1.next;
+                if (l2 != null)
+                    l2 = l2.next;
+
+
+            }
+
+            return head.next;
     }
 }
